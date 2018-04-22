84ee03773e1cf7c89d112754f64ee284954d6a59
dhaval-khatri1996 <dhavalkhatri1996@gmail.com>
2018-04-22 11:07:12 +0530

    GUI Features added

--- main.py
+++ main.py
@@ -1,11 +1,66 @@
-from treeGenerater import generateTree
-import word  
+import nltk
+import tkinter
 
-words=[]
-words.append(word.Word(0,"I","DET"))
-words.append(word.Word(-10,"don't","VER"))
-words.append(word.Word(8,"like","VER"))
-words.append(word.Word(0,"you","ADV"))
+from treeGenerator import generateTree
+from createDictionary import getDataset
+import word 
 
-generateTree(words)
 
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+def handlePunctuation(sentence):
+    return (sentence.replace(",", " ,").
+            replace(".", " .").
+            replace("!"," !").
+            replace("?"," ?").
+            replace(":"," ").
+            replace(";"," ").
+            replace("/"," ")).lower().split()
+
+#Get the list of words and their polarity   
+
+def generateResults(input,message):
+    result = "Rating : "
+    count = 1
+
+    if(input=='' or input==None):
+        return
+    
+    sentences = tokenizer.tokenize(input.get("1.0",'end-1c'))
+    
+    for sentence in sentences:
+        #Split the sentence into word
+        wordList = nltk.pos_tag(handlePunctuation(sentence),tagset='universal' )
+        words=[]
+    
+        for i  in wordList:
+            print(i)
+            if ( i[0] in wordDataset):
+                words.append(word.Word(i[0],wordDataset[i[0]],i[1]))
+            else:
+                words.append(word.Word(i[0],0,"X"))
+        ans = generateTree(words)
+        if(ans>10):
+            ans=10
+        elif(ans<-10):
+            ans=-10
+        result += "\nSentence{} = {}".format(count , ans )
+        count+=1
+    
+    message.configure(text=result)
+
+wordDataset = getDataset()
+
+window =tkinter.Tk(className = 'Vichar')
+window.title("Vichar")
+window.geometry('800x500')
+var = tkinter.StringVar()
+label1 = tkinter.Message( window, text="Enter the sentence to be rated :",width=200,relief=tkinter.RAISED )
+label1.place(x=5,y=3)
+rating = tkinter.Message( window, text="Rating : ",width=100)
+rating.place(x=0,y=55)
+sentenceInputHolder = tkinter.Text(window,width=70,height=3)
+sentenceInputHolder.place(x=200,y=2)
+button = tkinter.Button(window, text ="Get Rating", command = lambda: generateResults(sentenceInputHolder,rating))
+button.place(x=5,y=30)
+window.mainloop()
